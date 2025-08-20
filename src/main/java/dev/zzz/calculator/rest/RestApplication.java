0ff3e6ab5743279166211717d24dc924a381d881
TheGreatKamus <emiliofloresga@gmail.com>
2025-08-20 14:04:07 -0500

    Inicio de proyecto

---
+++ src/main/java/dev/zzz/calculator/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package dev.zzz.calculator.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
